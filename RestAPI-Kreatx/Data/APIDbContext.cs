755bbab912a90a0fb57a69ec2e6a36f1a36e0faf
undefined <45341025+nikolliervin@users.noreply.github.com>
2022-12-15 19:54:31 +0100

    Adding DbContext

---
+++ RestAPI-Kreatx/Data/APIDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestAPI_Kreatx.Data
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- RestAPI-Kreatx/Startup.cs
+++ RestAPI-Kreatx/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RestAPI_Kreatx.Data;
 
 namespace RestAPI_Kreatx
 {
@@ -26,7 +21,7 @@ namespace RestAPI_Kreatx
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<APIDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
